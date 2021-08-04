6ee498ec63ce504860b32aafcf540e34aafebbdc
MaysenTG <greenwood.maysen@gmail.com>
2021-08-04 21:19:07 +1200

    Change formatting for canvas. Create logic for better performing graph/calculations

--- script.js
+++ script.js
@@ -7,20 +7,36 @@ var contribution;
 var exponentialValues = [];
 var config;
 var graphLabel;
-var ctx = $('#exponentialGrowthChart').get(0).getContext('2d');
+window.chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(231,233,237)'
+  };
+  
+var ctx = $('#exponentialGrowthChart').get(0).getContext('2d', {alpha: false});
 
 
 // Variables for chart
 var labels = [];
 const options = {
     scales: {
-        ticks: {
-            beginAtZero: true,
-        },
-
-        myScale: {
-            position: "left",
-        }
+        y: {
+            min: 1,
+            max: 10,
+        },  
+    },
+    scale: {
+        pointLabels: {
+            fontSize: 24,
+        },   
+    },
+    
+    hover: {
+        mode: 'nearest',
     },
 
     plugins: {
@@ -31,7 +47,7 @@ const options = {
                     size: 40
                 }
             }
-        }
+        }     
     },
     responsive: true,
 }
@@ -41,11 +57,13 @@ var myChart = new Chart(ctx, {
     data: {
         labels: [],
         datasets: [{
-            label: 'My First Dataset',
+            label: 'Compound Growth',
             data: [],
-            //fill: false,
+            fill: false,
+            backgroundColor: 'rgb(255, 255, 255)',
             borderColor: 'rgb(75, 192, 192)',
-            tension: 0.1
+            tension: 0.1,
+            
         }]
     },
     options: options,
@@ -53,6 +71,12 @@ var myChart = new Chart(ctx, {
 
 jQuery("#submit-btn").on("click", function (e) {
     e.preventDefault();
+    capital = 0;
+    numyears = 0;
+    interest = 0;
+    contribution = 0;
+    exponentialValues = [];
+    
     capital = $("#initialcapital").val();
     numyears = $("#numyears").val();
     interest = $("#interest").val();
@@ -62,7 +86,7 @@ jQuery("#submit-btn").on("click", function (e) {
     $("div").getLabels();
     $("div").calculateGrowth();
     $("div").createUserChart();
-    $('#finalResult').text(exponentialValues[exponentialValues.length - 1]);
+    $(".exponentialGrowthChart").css('opacity', '1');
 });
 
 
@@ -91,7 +115,11 @@ jQuery("#submit-btn").on("click", function (e) {
     $.fn.createUserChart = function () {
         myChart.data.labels = labels;
         myChart.data.datasets[0].data = exponentialValues;
-
+        
+        myChart.options.scales.y.min = Math.round(capital);
+        myChart.options.scales.y.max = Math.round(exponentialValues[exponentialValues.length-1]);
+        
+        
         myChart.update();
     };
 })(jQuery);
